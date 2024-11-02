6d557d08e55fd879d4cdec807c17bea3c696f3dc
Pratham-Pendam <83838635+Pratham-Pendam@users.noreply.github.com>
2024-11-02 23:30:26 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 import './App.css'
 import { RouterProvider, createBrowserRouter } from 'react-router-dom'
 import  Home  from "./components/Home"
-import Login from "./components/auth/login"
+import Login from "./components/auth/Login"
 import Signup from './components/auth/Signup'
 import Jobs from  './components/Jobs'
 import Browse from  './components/Browse'
