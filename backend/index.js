9865489b2802f21d3caad1237f9e47d182c170f1
Pratham-Pendam <83838635+Pratham-Pendam@users.noreply.github.com>
2025-07-05 03:20:49 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -17,7 +17,7 @@ app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(cookieParser());
 const corsOptions = {
-    origin:'http://localhost:5173',
+    origin:'https://jobify-2-1qe6.onrender.com',
     credentials:true
 }
 // app.use(function(req, res, next) {
